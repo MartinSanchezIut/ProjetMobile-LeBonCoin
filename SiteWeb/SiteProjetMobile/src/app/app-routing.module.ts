dad37c93aa55a01212036e292402ba554ba4286a
MartinSanchezIut <martin.sanchez.iut@gmail.com>
2022-04-20 16:19:33 +0200

    interfaces acceuil

--- SiteWeb/SiteProjetMobile/src/app/app-routing.module.ts
+++ SiteWeb/SiteProjetMobile/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ConnexionComponent } from './connexion/connexion.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 const routes: Routes = [
+  { path: 'home', component: HomepageComponent },
   { path: 'connexion', component: ConnexionComponent }
 ];
 
