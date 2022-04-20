4b12af2ac5b314c34d8e3d17a8a530a971355c9c
Gaetan Romero <gaetan.romero@etu.umontpellier.fr>
2022-04-20 15:09:48 +0200

    sorting

--- SiteWeb/SiteProjetMobile/src/app/userauthentification.service.ts
+++ SiteWeb/SiteProjetMobile/src/app/userauthentification.service.ts
@@ -1,15 +1,13 @@
 import { Injectable } from '@angular/core'
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { User } from './user';
+import { User } from './Models/user';
 import { Observable } from 'rxjs';
-
 
 @Injectable({
   providedIn: 'root'
 })
 
-export class ConnexionService {
-
+export class UserauthentificationService {
   private urlBase: string = 'http://localhost:8888/';
 
   constructor(private http: HttpClient) {}
