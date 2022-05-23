809009a1b0107da953dfa1d9f1d21571f635386b
Matthieu <matthieutrinquart2000@gmail.com>
2022-05-23 17:57:33 +0200

    plus d annonces

--- serveurannonce/src/main/java/com/example/serveurannonce/Data/UserData.java
+++ serveurannonce/src/main/java/com/example/serveurannonce/Data/UserData.java
@@ -56,17 +56,20 @@ public class UserData {
 
             Annonce ann = new Annonce("Velo","Tres beau velo",50,"23/05/2022","Bouche du Rhone","Montpellier" ,a.getId_user(),i1,nbvue,"Email","Vacances","Location et Gites");
             Annonce ann1 = new Annonce("Marteau","Tres beau Marteau",50,"23/05/2022","Hérault","Montpellier" ,ap.getId_user(),i2,nbvue,"Email","Bricolage","Marteau");
-            Annonce ann2 = new Annonce("Phone","Tres beau téléphone",50,"23/04/2022","Bouche du Rhone","Miramas" ,a.getId_user(),i3,nbvue,"Email","Multimedia","Telephone");
-            Annonce ann3 = new Annonce("ordinateur","Tres beau ordinateur",50,"23/04/2022","Bouche du Rhone","Eyguières" ,a.getId_user(),i4,nbvue,"Email","Sport","velo");
-            Annonce ann4 = new Annonce("bateau","Tres beau bateau",50,"23/04/2022","Bouche du Rhone","Salon" ,a.getId_user(),i5,nbvue,"Email","Sport","velo");
-            Annonce ann5 = new Annonce("cafetière","Tres beau cafetière",50,"23/04/2022","Ile est Vilaine","Renne" ,a.getId_user(),i6,nbvue,"Email","Sport","velo");
-            Annonce ann6 = new Annonce("Eponge","Tres beau Eponge",50,"23/04/2022","Finistère","Brest" ,a.getId_user(),i7,nbvue,"Email","Sport","velo");
+            Annonce ann2 = new Annonce("Phone","Tres beau téléphone",50,"23/05/2022","Bouche du Rhone","Miramas" ,a.getId_user(),i3,nbvue,"Email","Multimedia","Telephone");
+            Annonce ann3 = new Annonce("ordinateur","Tres beau ordinateur",50,"23/0452022","Bouche du Rhone","Eyguières" ,a.getId_user(),i4,nbvue,"Email","Sport","velo");
+            Annonce ann4 = new Annonce("bateau","Tres beau bateau",50,"23/05/2022","Bouche du Rhone","Salon" ,a.getId_user(),i5,nbvue,"Email","Sport","velo");
+            Annonce ann5 = new Annonce("cafetière","Tres beau cafetière",50,"23/05/2022","Ile est Vilaine","Renne" ,a.getId_user(),i6,nbvue,"Email","Sport","velo");
+            Annonce ann6 = new Annonce("Eponge","Tres beau Eponge",50,"23/05/2022","Finistère","Brest" ,a.getId_user(),i7,nbvue,"Email","Sport","velo");
 
             ann.setAnn1(a);
             ann1.setAnn1(ap);
+            ann2.setAnn1(a);
+            ann3.setAnn1(ap);
+            ann4.setAnn1(a);
+            ann5.setAnn1(ap);
+            ann6.setAnn1(a);
           //  ann.addAnn3(c);
-            annonce.save(ann);
-           annonce.save(ann1);
             //annonce.save(ann6);
           // annonce.save(ann2);
          //   annonce.save(ann3);
@@ -114,6 +117,11 @@ public class UserData {
             a.addsauvegarde(ann1);
             annonce.save(ann);
             annonce.save(ann1);
+            annonce.save(ann2);
+            annonce.save(ann3);
+            annonce.save(ann4);
+            annonce.save(ann5);
+            annonce.save(ann6);
             annonceur_part.save(a);
             annonceur_pro.save(ap);
 
