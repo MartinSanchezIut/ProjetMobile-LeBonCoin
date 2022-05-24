5a3d9efc0254cffb6fbe3bf5e03262cd0d5a72b4
MartinSanchezIut <martin.sanchez.iut@gmail.com>
2022-05-24 16:22:08 +0200

    service annonce

--- SiteWeb_ProjetMobile/src/app/annonce.service.ts
+++ SiteWeb_ProjetMobile/src/app/annonce.service.ts
@@ -7,24 +7,102 @@ import { apiUrl } from './_Models/Utils' ;
   providedIn: 'root'
 })
 export class AnnonceService {
+/*
+	Liste des methodes du web service: 
+		"/AnnonceById/{id}"  		 OK
+		"/getAll"			 		 OK
+		"/Getsauvegarde/{id_user}"   OK
+		"/GetMesAnnonces/{id_user}"  OK
+		"/Recent"					 OK
+
+		"/Favoris"									OK
+		"/deleteannonce/{id_user}/{id_annonce}"	    OK
+
+		"/PutAnnonce" 		OK
+		"/PostAnnonce		OK
+
+		"/vu/{id_annonce}"  OK
+
+
+		"/RechercheTitre"   OK
+		"/Recherche"		OK
+
+		"/Getsauvegardeid/{id_user}"   
+		"/NbVuAnnonce/{id_annonce}"    OK
+		"/fraude"					   OK
+
+
+*/
 
   constructor(private http: HttpClient) {}
 
   private urlBase: string = apiUrl + '/annonce';
 
-  // Récupère une liste d'annonces
+  // Récupère une liste d'annonces ("/Recent", 
+  // 								"/GetMesAnnonces/{id_user}"
+  //								"/Getsauvegarde/{id_user}"
+  //								"/getAll")
   getListAnnonce(url : string) : Observable<any> {
       console.log("Récupération d'annonces ("+ url +").");
-
       return this.http.get<any>(this.urlBase + url);
     }
-
   // Récupère une annonce par son id
   getAnnonceById(id : number) : Observable<any> {
     console.log("Récupération de l'annonces "+ id +".");
-
     return this.http.get<any>(this.urlBase + "/AnnonceById/"+id);
   }
-  
+  // Récupère une annonce par son id
+  getNombreVueAnnonceById(id : number) : Observable<any> {
+    console.log("Récupération du nombre de vue de l'annonce "+ id +".");
+    return this.http.get<any>(this.urlBase + "/NbVuAnnonce/"+id);
+  }
 
+   // Ajouter une annonce au favoris d'un utilisateur 
+   ajouterAnnonceAuFavoris(userANDannoncelist : any) : Observable<any> {
+	console.log("Favori add user:"+ userANDannoncelist[0] + " annonce: " + userANDannoncelist[1]);
+	return this.http.post<any>(this.urlBase + "/Favoris", userANDannoncelist);
+   }
+   // Supprimer une annonce au favoris d'un utilisateur 
+   supprimerAnnonceAuFavoris(id_user : number, id_annonce : number) : Observable<any> {
+	console.log("Favori rm user:"+ id_user + " annonce: " + id_annonce);
+	return this.http.delete<any>(this.urlBase + "/deleteannonce/"+id_user+"/"+id_annonce)
+   }
+
+
+  // Ajouter une vue a l'annonce :id
+  ajouterVueAnnonce(id : number) : Observable<any> {
+      console.log("Ajout d'une vue à l'annonce "+ id +".");
+      return this.http.get<any>(this.urlBase + "/vu/"+id);
+   }
+
+   // Ajouter une annonce
+   ajouterAnnonce(annonce : any) : Observable<any> {
+	console.log("Ajout annonce "+ annonce );
+    return this.http.put<any>(this.urlBase + "/PutAnnonce", annonce);
+   }
+	// Editer une annonce
+	editerAnnonce(annonce : any) : Observable<any> {
+		console.log("editer annonce "+ annonce );
+		return this.http.put<any>(this.urlBase + "/PostAnnonce", annonce);
+	}
+
+
+
+	// Retourne une liste d'annonces qui on le titre :titre
+	rechercherParTitre(titre : string) : Observable<any>{
+		console.log("Recherche annonce :"+ titre );
+		return this.http.post<any>(this.urlBase + "/RechercheTitre", titre);
+	}
+	// Retourne une liste d'annonces qui collent avec recherche
+	rechercher(recherche : any) : Observable<any>{
+		console.log("Recherche annonce :"+ recherche );
+		return this.http.post<any>(this.urlBase + "/RechercheTitre", recherche);
+	}
+
+
+	// Signale une fraude
+	fraude(fraude : any) : Observable<any>{
+		console.log("Signaler fraude :"+ fraude );
+		return this.http.post<any>(this.urlBase + "/RechercheTitre", fraude);
+	}
 }
