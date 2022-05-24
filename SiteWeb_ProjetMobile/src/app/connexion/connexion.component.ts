079e01ba71e241a3c0744d7ff5eaa81caa803cff
Gaetan Romero <gaetan.romero@etu.umontpellier.fr>
2022-05-24 16:04:59 +0200

    nothing

--- SiteWeb_ProjetMobile/src/app/connexion/connexion.component.ts
+++ SiteWeb_ProjetMobile/src/app/connexion/connexion.component.ts
@@ -23,23 +23,6 @@ export class ConnexionComponent {
   //Permet de valider la connexion d'un utilisateur
   //Si les informations rentrées par l'utilisateur sont celles rendues par le serveur alors redirigé vers la page '/home'
   //Sinon un message d'alerte s'affiche disant que cet utilisateur n'existe pas
-
-  // submit(): void {
-  //   this.email = (<HTMLInputElement>document.getElementById('Identifiant')!).value;
-  //   this.password = (<HTMLInputElement>document.getElementById('password')!).value;
-  //   console.log("Recherche des identifiants pour la connexion");
-  //   this.connectingUser.connexion(this.email, this.password).subscribe(userconnected => {
-  //     if (userconnected != undefined) {
-  //       this.userconnected = userconnected; 
-  //       localStorage.setItem("user", JSON.stringify(this.userconnected));
-  //       this.router.navigate(['/home']); //Redirection 
-  //     } else {
-  //       alert("Cet utilisateur n'existe pas"); //Message d'alert
-  //     } 
-  //   });
-  //   this.userconnected.setEmail("*******");
-  // }
-
   submit(): void {
     this.email = (<HTMLInputElement>document.getElementById('Identifiant')!).value;
     this.password = (<HTMLInputElement>document.getElementById('password')!).value;
